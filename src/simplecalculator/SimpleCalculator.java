40b557e547f265fa90d3402a88e30d537335f45f
borislavkostov <borislavkostov97@gmail.com>
2016-08-24 15:43:37 +0300

    Plus minus calculator

---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplecalculator;
+
+/**
+ *
+ * @author templars1914
+ */
+public class SimpleCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ClaculatorFrame frame=new ClaculatorFrame();
+       frame.setVisible(true);
+    }
+    
+}
